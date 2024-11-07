50978119cd25e7754eafa8a18e04526d1ecc9ed2
Nando218 <fernando.delatorre.design@gmail.com>
2024-11-07 09:36:15 +0100

    search message

--- main.js
+++ main.js
@@ -12,49 +12,63 @@ function displayMovies(movies) {
   const movieSection = document.getElementById("movieSection");
   movieSection.innerHTML = ""; // Limpiar el contenido antes de agregar las películas
 
-  movies.forEach(movie => {
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('col-md-4', 'mb-4');
+  if (movies.length === 0) {
+    // Si no hay resultados, mostrar mensaje de "sin resultados"
+    const noResultsMessage = document.createElement('div');
+    noResultsMessage.classList.add('text-light');
+    noResultsMessage.style.display = 'flex';
+    noResultsMessage.style.justifyContent = 'center';
+    noResultsMessage.style.alignItems = 'center';
+    noResultsMessage.style.height = '60vh'; // Altura suficiente para centrar en el contenedor
+    noResultsMessage.style.fontSize = '1.8rem'; // Tamaño de texto más grande para visibilidad
+    noResultsMessage.innerHTML = `<p>No se han encontrado resultados para la búsqueda.</p>`;
+    movieSection.appendChild(noResultsMessage);
+  } else {
+    // Si hay películas, mostrarlas en el contenedor
+    movies.forEach(movie => {
+      const movieCard = document.createElement('div');
+      movieCard.classList.add('col-md-4', 'mb-4');
 
-    // Crea el HTML de la tarjeta
-    movieCard.innerHTML = `
-      <div class="card h-100" style="width: 24rem;">
-        <div class="position-relative">
-          <img src="${movie.poster}" class="card-img-top" alt="${movie.title} poster" style="height: 32rem; cursor: pointer;">
-          <div class="trailer-text" style="position: absolute; bottom: 10px; left: 10px; color: white; background-color: rgba(0, 0, 0, 0.7); padding: 5px; display: none;">
-            Ver el trailer
+      // Crea el HTML de la tarjeta
+      movieCard.innerHTML = `
+        <div class="card h-100" style="width: 24rem;">
+          <div class="position-relative">
+            <img src="${movie.poster}" class="card-img-top" alt="${movie.title} poster" style="height: 32rem; cursor: pointer;">
+            <div class="trailer-text" style="position: absolute; bottom: 10px; left: 10px; color: white; background-color: rgba(0, 0, 0, 0.7); padding: 5px; display: none;">
+              Ver el trailer
+            </div>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-title"><span class="h6">${movie.year}</span> . ${movie.length}</p>
+            <h6 class="card-title">${movie.director}</h6>
+            <p class="card-text mb-4">${movie.synopsis}</p>
           </div>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">${movie.title}</h5>
-          <p class="card-title"><span class="h6">${movie.year}</span> . ${movie.length}</p>
-          <h6 class="card-title">${movie.director}</h6>
-          <p class="card-text mb-4">${movie.synopsis}</p>
-        </div>
-      </div>
-    `;
+      `;
 
-    // Añade eventos de mouse para mostrar/ocultar el texto
-    const image = movieCard.querySelector('.card-img-top');
-    const trailerText = movieCard.querySelector('.trailer-text');
+      // Añade eventos de mouse para mostrar/ocultar el texto
+      const image = movieCard.querySelector('.card-img-top');
+      const trailerText = movieCard.querySelector('.trailer-text');
 
-    // Mostrar el texto al pasar el ratón
-    image.addEventListener('mouseenter', () => {
-      trailerText.style.display = 'block'; // Muestra el texto
+      // Mostrar el texto al pasar el ratón
+      image.addEventListener('mouseenter', () => {
+        trailerText.style.display = 'block'; // Muestra el texto
+      });
+
+      // Ocultar el texto al salir el ratón
+      image.addEventListener('mouseleave', () => {
+        trailerText.style.display = 'none'; // Oculta el texto
+      });
+
+      // Evento de clic en la imagen para abrir el enlace del tráiler en una nueva ventana
+      image.addEventListener('click', () => {
+        window.open(movie.trailer, '_blank'); // Abre el enlace del tráiler en una nueva pestaña
+      });
+
+      movieSection.appendChild(movieCard);
     });
-
-    // Ocultar el texto al salir el ratón
-    image.addEventListener('mouseleave', () => {
-      trailerText.style.display = 'none'; // Oculta el texto
-    });
-
-    // Evento de clic en la imagen para abrir el enlace del tráiler en una nueva ventana
-    image.addEventListener('click', () => {
-      window.open(movie.trailer, '_blank'); // Abre el enlace del tráiler en una nueva pestaña
-    });
-
-    movieSection.appendChild(movieCard);
-  });
+  }
 }
 
 // Función de búsqueda
